1617899f0d18b9474f8ff5ab0d33ce125c9940c9
Kirill Demidov <kademidov@gmail.com>
2016-03-31 14:13:20 +0300

    second fix change

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+    $('#owl-reviews').owlCarousel({
         loop:true,
         margin:20,
         nav:true,
@@ -19,4 +19,38 @@ $(document).ready(function(){
         }
     });
 
+     $('#owl-sertificate').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        navText:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
+
 });
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: 60.0679, lng: 30.3600},
+    scrollwheel: false
+  });
+
+  var image = '../img/icon-for-map.png';
+  var beachMarker = new google.maps.Marker({
+    position: {lat: 60.0679, lng: 30.3600},
+    map: map,
+    icon: image
+  });
+}
